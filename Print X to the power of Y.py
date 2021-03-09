9c4643e880b18b5a2895da40a7b9e6b79fbfe9eb
NeilCarvalho <65865858+NeilCarvalho@users.noreply.github.com>
2021-03-09 19:03:32 +0530

    Add files via upload

---
+++ Print X to the power of Y.py
@@ -0,0 +1,4 @@
+﻿a = int(input("Please enter a number(the base numeral):"))
+b = int(input("Please enter a number(the exponent):"))
+
+print(a, "to the power", b, "is", a ** b)
---
+++ numbers divisible by 3.py
@@ -0,0 +1,12 @@
+﻿vals = [int(i) for i in input("please enter a list of numbers:").split()]
+#print(type(vals))
+#print(len(vals))
+#print(type(len(vals))
+numbers_divisible_by3 = []
+i = 0
+k = int(len(vals))
+while i < k:
+    if vals[i] % 3 == 0:
+        numbers_divisible_by3.append(vals[i])
+    i = i + 1
+print("The numbers entered that are divisible by 3 are", numbers_divisible_by3)
